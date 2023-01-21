b25931162fef08bf64da94d13168ee52209a8f52
Oleh <popovich158@gmail.com>
2023-01-21 22:33:56 +0200

    creating empty project

---
+++ les-21/src/main/java/com/example/les21/Les21Application.java
@@ -0,0 +1,13 @@
+package com.example.les21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Les21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Les21Application.class, args);
+	}
+
+}
